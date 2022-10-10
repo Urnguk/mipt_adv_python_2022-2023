f480e231e9b0848c1a7a76c9cf8a77ba5f2c2a4e
Urnguk <spiri-o-wate@mail.ru>
2022-10-10 18:28:21 +0300

    Update generator.py

--- Lesson_6/generator.py
+++ Lesson_6/generator.py
@@ -1,20 +1,35 @@
-# A = (int(x) for x in input().split())
-# for i in A:
+a = list()
+
+
+# def squares():
+#     res = 0
+#     while True:
+#         yield res**2
+#         res += 1
+#         if res > 100:
+#             return
+
+
+# print(squares())
+
+
+# def my_range(start, finish=None, step=1):
+#     if step == 0:
+#         raise ValueError
+#     if finish is None:
+#         start, finish = 0, start
+#     yield start
+#     while (finish - start) * (finish - start - step) > 0:
+#         start += step
+#         yield start
+
+
+# for i in my_range(4, 1, -1):
 #     print(i)
 
 
-def squares():
-    res = 0
-    while True:
-        if res == 100:
-            break
-        yield res**2
-        res += 1
 
-
-
-
-for i in squares():
-    print(i)
-#    if i > 10000:
-#        break
+# for i in squares():
+#     print(i)
+    # if i > 10000:
+    #     break
