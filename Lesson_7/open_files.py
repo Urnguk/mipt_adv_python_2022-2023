f7a071ee91a4d74a82f27e88bc2f88021f08ae22
Urnguk <spiri-o-wate@mail.ru>
2022-10-21 17:42:31 +0300

    Materials updated in Lesson_7

--- Lesson_7/open_files.py
+++ Lesson_7/open_files.py
@@ -1,9 +1,14 @@
-f = open("test.txt", mode='r+')
-print(f)
-print(f.readline(), end="")
-print(f.tell())
-f.seek(0)
-for line in f:
-    print(line, end="")
-
-f.close()
+with open("Test.txt", mode="r+") as f:
+    print(f)
+    f.seek(0)
+    f.write("cccc")
+    # print(f.tell())
+    # print(f.readline())
+    # print(f.tell())
+    # print(f.readline())
+    # print(f.tell())
+    # f.seek(4)
+    # print(repr(f.read(1)))
+    # for x in f:
+    #     print(x, end="")
+print()
--- Lesson_7/serialization.py
+++ Lesson_7/serialization.py
@@ -1,7 +1,35 @@
 import pickle
+import numbers
+
+
+class Pair:
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    def get_x(self):
+        return self._x
+
+    def get_y(self):
+        return self._y
+
+    def __add__(self, other):
+        if isinstance(other, numbers.Number):
+            return Pair(self._x + other, self._y + other)
+        if isinstance(other, Pair):
+            return Pair(self._x + other.get_x(),
+                        self._y + other.get_y())
+        return False
+
+    def __str__(self):
+        return str(self._x) + " " + str(self._y)
+
+
+p = Pair(2, 3)
 d = {1: [3], "acc": (3, 7)}
 with open("pickle_data.bin", mode="wb") as f:
-    pickle.dump(d, f)
+    pickle.dump(p, f)
+
 with open("pickle_data.bin", mode="rb") as f:
     a = pickle.load(f)
 print(type(a), a)
